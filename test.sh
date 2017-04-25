5c30016ada198ff38c22ed3e264bad4248d3ad1f
Taishi Watanabe <taishi@icn: ktm-0808mc@eis.hokudai.ac.jp>
2017-04-25 14:07:04 +0900

    add0425

--- test.sh
+++ test.sh
@@ -13,10 +13,11 @@ python3 minsumsim.py <snr.txt
 python3 format10to2.py
 python3 format10to16.py
 erb test_test.erb.v > test_test.v
+erb sram.erb.v > sram.v
 erb Ctrl.erb.v > Ctrl.v
 erb add.erb.v > add.v
 erb row.erb.v >row.v
-vlog +acc -lint test_test.v Ctrl.v row.v  add.v
+vlog +acc -lint test_test.v Ctrl.v row.v  add.v sram.v 
 vsim -c -do  'run -all ; exit ' test_test
 python3 format2to10.py
 
